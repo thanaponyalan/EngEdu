d23b2629c312ca564bd404565f95500c67f216a8
thanaponyalan <thanaponyalan@gmail.com>
2018-08-24 15:54:16 +0700

    3rd year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -14,6 +14,7 @@ namespace EngEdu
             Console.WriteLine("Year 2560 semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2561 semester 1, I have to regrade on some course.");
             Console.WriteLine("Year 2561 semester 2, I learn to understand computer systems");
+            Console.WriteLine("Year 2562 semester 1, I learn to use microcontroller");
         }
     }
 }
