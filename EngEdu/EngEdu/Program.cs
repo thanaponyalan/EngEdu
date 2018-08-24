901c9981ecdddb9449510008c73447a5725cf1df
thanaponyalan <thanaponyalan@gmail.com>
2018-08-24 15:24:59 +0700

    first year semester 1

---
+++ EngEdu/EngEdu/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngEdu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year 2560 semester 1, I am and Engedu freshmen");
+        }
+    }
+}
